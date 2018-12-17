71a18598161250c0408a74bd88cd86fe4ac62318
Amal Majeed <amalmajeed@Amals-MacBook-Air.local>
2018-12-17 22:16:30 +0530

    Template classes

--- temp.cpp
+++ temp.cpp
@@ -3,6 +3,9 @@
 
 using namespace std;
 
+
+//// TEMPLATE FUNCTION 
+
 template <typename T>
 
 T add(T a,T b)
@@ -11,12 +14,44 @@ T add(T a,T b)
 
 }
 
+///TEMPLATE CLASS
+
+
+template <class A>
+class addition
+{
+	public:
+		A a[2];
+		A add();
+		addition();
+
+};
+
+template <class A>
+addition<A>::addition()
+{
+	cin>>a[0]>>a[1];
+}
+
+template <class A>
+A addition<A>::add()
+{
+	return a[0]+a[1];
+}
+
+
 int main()
 {
- int a=1,b=2;
- float x=10.2,y=7.3;
- cout<<add<int>(a,b)<<"\n";
- cout<<add<float>(x,y)<<"\n";
+ // int a=1,b=2;
+ // float x=10.2,y=7.3;
+ // cout<<add<int>(a,b)<<"\n";
+ // cout<<add<float>(x,y)<<"\n";
+cout<<"INT\n";
+addition<int> obj1;
+cout<<"FLOAT\n";
+addition<float> obj2;
+cout<<obj1.add()<<"\n"<<obj2.add();
+
 
 return 0;
 }
